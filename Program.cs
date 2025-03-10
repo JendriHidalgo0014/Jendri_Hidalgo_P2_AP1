062c17cb0842eb9a8364bd9c6e60dc1a6bf5f403
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-03-10 19:52:18 -0400

    Se agregó el constructor para usar el ConnectionString y tambien se realizó la inyeccion del Service.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
 using Jendri_Hidalgo_P2_AP1.Components;
+using Jendri_Hidalgo_P2_AP1.Services;  
+using Jendri_Hidalgo_P2_AP1.DAL;
+using Jendri_Hidalgo_P2_AP1.Models;
+using BlazorBootstrap;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+//Obtenemos el constructor para usarlo en el contexto.
+var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
+
+builder.Services.AddDbContextFactory<Context>(options => options.UseSqlServer(ConStr));
+builder.Services.AddBlazorBootstrap();
+
+//Inyeccion del service.
+builder.Services.AddScoped<CiudadesService>();
+builder.Services.AddScoped<EncuestaService>();
+builder.Services.AddScoped<CiudadesDetalleService>();
+
+
 
 var app = builder.Build();
 
@@ -26,3 +44,4 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+
