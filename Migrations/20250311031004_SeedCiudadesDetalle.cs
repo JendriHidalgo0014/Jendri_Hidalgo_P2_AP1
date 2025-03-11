49ff2ccd32fe9efe0dcaf79b912f075db364c455
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-03-11 00:47:52 -0400

    Se actualizó la base de datos

---
+++ Migrations/20250311031004_SeedCiudadesDetalle.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jendri_Hidalgo_P2_AP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCiudadesDetalle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
